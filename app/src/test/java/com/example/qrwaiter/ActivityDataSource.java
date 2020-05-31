0bcb6c9f39ece00b856f1c4d104592589273d6bb
Ludyyy <d.ludwig18@web.de>
2020-05-31 16:52:11 +0200

    AWS database

---
+++ app/src/test/java/com/example/qrwaiter/ActivityDataSource.java
@@ -0,0 +1,9 @@
+package com.example.qrwaiter;
+
+import android.os.AsyncTask;
+
+public class ActivityDataSource extends AsyncTask<String, Void, String> {
+    protected String doInBackground(String... params) {
+        return null;
+    }
+}
